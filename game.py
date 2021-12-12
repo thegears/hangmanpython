3367b291edc279cd090b7c1398a54f5cee3f097b
Semih Eroğlu <65253167+thegears@users.noreply.github.com>
2021-12-12 22:37:11 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,9 +1,27 @@
-word = "metallica";
+import os;
+import random;
+import platform;
+
+if "Windows" in platform.platform():
+	clear_command = "cls";
+else:
+	clear_command = "clear";
+
+language = input("Your Language (en , tr) : ").lower();
+while language != "en" and language != "tr":
+	language = input("Your Language (EN , TR) : ").lower();
+
+with open(f"words_{language}.txt",mode="r") as isimler:
+	word_list = isimler.read().split("\n");
+	isimler.close();
+
+word = word_list[random.randint(0,2445)].upper();
 knowing_letter = [];
 hp = 10;
 isWinner = False;
 
 while hp > 0:
+	os.system(clear_command);
 	if len(set(word)) == len(set(knowing_letter)):
 		isWinner = True;
 		break;
@@ -13,7 +31,14 @@ while hp > 0:
 		else:
 			print("-");
 	print(f"Your HP : {hp}");
-	guess = input("Your Guess : ").lower();
+	guess = input("Your Guess : ");
+	if guess == "answer":
+		print(word);
+		break;
+	if guess == "i":
+		guess = "İ"
+	else:
+		guess = guess.upper();
 
 	if guess in word:
 		knowing_letter.append(guess);
@@ -21,6 +46,14 @@ while hp > 0:
 		hp -= 1;
 
 if isWinner == False:
+	os.system(clear_command);
+	print(f"Word : {word}");
+	print("--------------- ---------------");
 	print("You Lost :(");
+	print("--------------- ---------------");
 else:
+	os.system(clear_command);
+	print(f"Word : {word}");
+	print("--------------- ---------------");
 	print("You Won :)");
+	print("--------------- ---------------");
